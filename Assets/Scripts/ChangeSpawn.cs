d323e2c9d6006729a5ffd6dcce7ca1989a850086
REz <rez20919@yahoo.com>
2018-10-27 18:44:04 +0100

    2_Obstacles

---
+++ Assets/Scripts/ChangeSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSpawn : MonoBehaviour {
+
+    private Rigidbody2D thisRigidbody;
+    [SerializeField]
+    private Transform spawnPoint;
+    private int deltaSpawn = 5;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void MoveSpawn(){
+
+        thisRigidbody.velocity += Vector2.up * deltaSpawn;
+    }
+}
