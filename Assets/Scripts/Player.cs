83673c903d5dc11b8c1241a217c85c70747a811a
James Adey <jamesadey91@gmail.com>
2018-10-27 15:44:15 +0100

    player falling

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D thisRigidbody;
+
+    void Awake()
+    {
+        thisRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Respawn(Vector2 pos, float rot)
+    {
+        thisRigidbody.position = pos;
+        thisRigidbody.rotation = rot;
+        //thisRigidbody.velocity = Vector2.zero;
+        thisRigidbody.angularVelocity = 0;
+    }
+}
